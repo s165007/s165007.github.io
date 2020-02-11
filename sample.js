7bb307615dcd20af1deaf1665caaafa85ff2bf08
s165007 <39042876+s165007@users.noreply.github.com>
2020-02-11 17:09:45 +0900

    Update sample.js

--- sample.js
+++ sample.js
@@ -34,8 +34,8 @@ var options = document.querySelectorAll("#kuni option");
 function onYouTubeIframeAPIReady() {
     ytPlayer = new YT.Player(ytArea, {
         videoId: ytID,
-        width: 440, 
-        height: 260,
+        width: 140, 
+        height: 80,
         playerVars: {
             loop: 1, // ループの設定
             playsinline: 1
